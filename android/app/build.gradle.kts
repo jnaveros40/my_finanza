1d9260c243048a7dadf3900f25ddd6fbb09ba39d
jnaveros40 <jnaveros40@gmail.com>
2025-07-10 10:46:56 -0500

    Update Android build configuration: change namespace, set NDK version, and update versionCode and minSdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,13 +14,14 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.mis_finanza"
+    namespace = "com.jnaveros.misfinanza"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,9 +42,9 @@ android {
         applicationId = "com.jnaveros.misfinanza"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 2
+        versionCode = 3
         versionName = flutter.versionName
     }
 
@@ -52,6 +53,11 @@ android {
             signingConfig = signingConfigs.getByName("release")
         }
     }
+}
+
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 flutter {
